5c4c0ce06ca2918b0ba9bae7d6f3e04be38c1750
mark <mark.kors@gmail.com>
2018-02-20 18:11:09 +0100

    init

---
+++ form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>formulier</title>
+</head>
+<body>
+
+<form action="form.php" method="post">
+    <table>
+        <tr>
+            <td>naam</td>
+            <td><input type="text" name="naam"></td>
+        </tr>
+        <tr>
+            <td>adres</td>
+            <td><input type="text" name="adres"></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type="submit" name="submit" value="submit"></td>
+        </tr>
+    </table>
+</form>
+
+</body>
+</html>
---
+++ verwerken.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mark
+ * Date: 20/02/2018
+ * Time: 18:05
+ */
+
+?>
+
+
